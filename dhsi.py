b175493cc56ec0dbcc2c71a1b36feab35f41fec0
Jonathan Reeve <jon.reeve@gmail.com>
2019-06-05 10:56:13 -0700

    fix environment vars

--- dhsi.py
+++ dhsi.py
@@ -4,19 +4,17 @@ import json
 import time
 
 import tweepy
-from secrets import *
+#from secrets import *
 
-#from os import environ
-#CONSUMER_KEY = environ['CONSUMER_KEY']
-#CONSUMER_SECRET = environ['CONSUMER_SECRET']
-#ACCESS_KEY = environ['ACCESS_KEY']
-#ACCESS_SECRET = environ['ACCESS_SECRET']
+from os import environ
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
-
-#war = open('war-and-peace.txt').read()
 
 lastTime = 0
 
